888da3f3843333a6544c7a2860eda5dd57ab7171
Jeremy <200618875@student.georgianc.on.ca>
2025-08-12 13:13:41 -0400

    Question 1 done

---
+++ src/main/java/com/example/finalexam/Customer.java
@@ -0,0 +1,11 @@
+package com.example.finalexam;
+
+import java.util.Arrays;
+
+public class Customer {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private Arrays products;
+}
